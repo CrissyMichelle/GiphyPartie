b9cb9ef57cacb7636874465bb8035c443adde056
Crissy Cabrera <crissymichelle@proton.me>
2023-05-05 05:59:26 -1000

    Append gifs to DOM and added a delete button

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ console.log("Let's get this party started!");
 let apiKey = "K5Z7Isog7oI0Et7L0pcOTbYCTrBUkM36";
 const form = document.querySelector('#searchForm');
 const input = document.querySelector('#search')
-document.addEventListener("DOMContentLoaded", init);
-function init() {
-    form.addEventListener("submit", ev => {
-        ev.preventDefault();
-        getRandomGif(input.value);
-    })
-}
+const div = document.querySelector('#gifList');
+const search = document.querySelector('#btnSearch');
+const remove = document.querySelector('#deleteBtn');
+
+search.addEventListener("click", ev => {
+    ev.preventDefault();
+    getRandomGif(input.value);
+
+})
+remove.addEventListener("click", ev => {
+    console.log("Party's over?");
+})
 async function consolationGig() {
     const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=1&offset=0&rating=g&lang=en&q=umhello`);
     const img = document.querySelector('#gif');
@@ -19,9 +24,9 @@ async function getRandomGif(term) {
         const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=1&offset=0&rating=g&lang=en&q=${term}`;
         const res = await axios.get(url);
         let str = document.getElementById("search").value.trim();
-        console.log(res.data.data[0].images.downsized.url);
-        const img = document.querySelector('#gif');
-        img.src = res.data.data[0].images.downsized.url;
+        const newImg = document.createElement('img');
+        newImg.src= res.data.data[0].images.downsized.url;
+        div.append(newImg);
     } catch(e) {
         alert("um, WHAT HAPPENED? i mean, did you search for something truly arcane?? Maybe the internet's down");
         consolationGig();
